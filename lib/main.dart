244b566efd697dec4aa216448df557eb40d675a4
Kun <clabstar0623@gmail.com>
2022-07-02 17:12:50 +0900

    return to taecho viliage

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,7 @@
-import 'package:chang_mini/pages/splash_page.dart';
-import 'package:chang_mini/services/bank_service.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => BankService(prefs)),
-      ],
-      child: const MyApp(),
-    ),
-  );
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -25,10 +10,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      home: SplashPage(),
-    );
+    return Scaffold();
   }
 }
