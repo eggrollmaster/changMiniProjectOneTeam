177f3af2eead2c69c5a4af5106068eb4367292b8
eggrollmaster <93654169+eggrollmaster@users.noreply.github.com>
2022-07-05 22:19:44 +0900

    Create splash_page.dart

---
+++ lib/splash_page.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+import 'dart:ffi';
+
+import 'package:chang_mini/main.dart';
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      Duration(seconds: 2),
+      () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const HomePage(),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('assets/toss_bank.png'),
+      ),
+    );
+  }
+}
